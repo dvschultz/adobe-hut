d6bc84ba929e5a29a42cc59fac246280b0a0cb98
Schultz <derrick@bustbright.com>
2016-02-24 16:07:40 -0500

    example pathfinder script command

---
+++ ai/pathfinder add-expand.jsx
@@ -0,0 +1,1 @@
+﻿/*    this programtically selects all shapes, groups them, then uses the pathfinder add command, then exapnds them    broken out here, but better used in conjunction with a larger script*/app.executeMenuCommand('selectall'); app.executeMenuCommand('group'); app.executeMenuCommand ('Live Pathfinder Add');  app.executeMenuCommand ('expandStyle'); 
